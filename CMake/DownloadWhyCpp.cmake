f2483e8d2b7f0c88a796f1661727770f1ce4c56f
Ilya Siganov <ilya.blan4@gmail.com>
2019-03-01 00:15:23 +0600

    apply linux fonts fix

--- CMake/DownloadWhyCpp.cmake
+++ CMake/DownloadWhyCpp.cmake
@@ -4,8 +4,8 @@ project(whycpp-download NONE)
 
 include(ExternalProject)
 ExternalProject_Add(whycpp
-        URL               https://github.com/senior-sigan/WHY_CPP/archive/v1.3.tar.gz
-        URL_MD5           "bd086709a34fb5006dca8155526a8f5d"
+        URL               https://github.com/senior-sigan/WHY_CPP/archive/v1.3.1.tar.gz
+        URL_MD5           "8a156ac724ab4f047883019165c3ad9e"
         SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/whycpp-src"
         BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/whycpp-build"
         CONFIGURE_COMMAND ""
